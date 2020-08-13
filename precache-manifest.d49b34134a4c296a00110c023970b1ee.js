58a866a2cf051a8f76aeda7a43959a6bc56ece63
abc <abc@example.com>
2020-08-13 12:09:18 +0530

    Updates

---
+++ precache-manifest.d49b34134a4c296a00110c023970b1ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "79a8dd2951950fd211a5",
+    "url": "/joke-app/static/css/main.1aee4e64.chunk.css"
+  },
+  {
+    "revision": "79a8dd2951950fd211a5",
+    "url": "/joke-app/static/js/main.782a6fde.chunk.js"
+  },
+  {
+    "revision": "2b19d7cfbc214500a6cb",
+    "url": "/joke-app/static/js/runtime~main.433746e7.js"
+  },
+  {
+    "revision": "38374272f8552f0ceb2b",
+    "url": "/joke-app/static/js/2.49493aa6.chunk.js"
+  },
+  {
+    "revision": "e8511fdd4d2c0a3eb534a325c4096e98",
+    "url": "/joke-app/index.html"
+  }
+];
